f72d72ee9709949b38966bd3c8b809e68cb5804e
Michelle Charron <michelle.charron93@gmail.com>
2021-01-03 15:13:00 -0700

    Injected Modal into card for Keeps

--- wwwsrc/src/components/KeepsComponent.vue
+++ wwwsrc/src/components/KeepsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="keep card bg-light text-white align-text-bottom">
+  <div class="keep card bg-light text-white align-text-bottom" data-toggle="modal" data-target="#exampleModal">
     <img :src="keepProp.img" alt="" class="card-img">
     <div class="card-img-overlay row justify-content-between align-items-end">
       <h3 class="card-title">
@@ -10,6 +10,37 @@
         UserIcon here
       </div>
     </div>
+    <div class="modal fade"
+         id="exampleModal"
+         tabindex="-1"
+         role="dialog"
+         aria-labelledby="exampleModalLabel"
+         aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              Modal title
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +48,7 @@
 import { computed } from 'vue'
 import { profileService } from '../services/ProfileService'
 import { useRouter } from 'vue-router'
+import { AppState } from '../AppState'
 export default {
   name: 'Keep',
   props: {
@@ -25,6 +57,7 @@ export default {
   setup(props) {
     const router = useRouter()
     return {
+      profile: computed(() => AppState.profile),
       keep: computed(() => props.keepProp),
       seeProfilePage() {
         router.push({ name: 'Profile', params: { id: props.keepProp.creatorId } })
